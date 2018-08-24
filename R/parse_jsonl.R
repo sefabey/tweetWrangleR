f8d6fae36218eab0fa830114f371cc3d9633248e
Sefa Ozalp <sefabey@gmail.com>
2018-08-24 14:11:25 +0100

    add export_as_csv argument to parse_jsonl(), update test

--- R/parse_jsonl.R
+++ R/parse_jsonl.R
@@ -6,6 +6,7 @@
 #' @param tweet_colnames Character vector. The colnames you want in the output tibble. In the object tweetWrangleR::tweet_cols, 99 opinionated colnames provided as default but users can provide a custom colname vector by add new ones or drop existing ones.
 #' @param tweet_lang Character. Filter tweets only in one language. Default is "en" for English language. Users can provide other languages as well.
 #' @param filter_term_regex Character. Accepts regex. Default is "muslim|islam". You can the keyword(s) you've used betwen quotation marks while filtering data. If you want to provide multiple keywords, put them between speech marks and separate by '|' (which means 'or').
+#' @param export_as_csv Logical. If **TRUE**, it will export parsed data as a csv file within the same path as the .jsonl file. If **FALSE** (default), it will return a tibble.
 #'
 #' @return Always returns a tibble with the columns provided in the tweet_colnames argument. Default alwayns returns a tibble with 99 columns. If there are values that are missing in the source .jsonl file, fills NA.
 #'
@@ -13,7 +14,7 @@
 #'
 #' @examples parse_jsonl("user/desktop/tweets.jsonl")
 #'
-parse_jsonl <- function (jsonl_path, tweet_colnames=tweet_cols, tweet_lang='en', filter_term_regex="muslim|islam") {
+parse_jsonl <- function (jsonl_path, tweet_colnames=tweet_cols, tweet_lang='en', filter_term_regex="muslim|islam", export_as_csv=FALSE) {
   stopifnot(stringr::str_detect(jsonl_path,"\\.jsonl$"))
   json <- ndjson::stream_in(jsonl_path, cls = "tbl") %>%
     filter(lang==tweet_lang) %>%
@@ -24,4 +25,9 @@ parse_jsonl <- function (jsonl_path, tweet_colnames=tweet_cols, tweet_lang='en',
     select(text_long, id_str, everything()) %>%
     filter(stringr::str_detect(string = text_long, pattern = regex(filter_term_regex,ignore_case = T))) %>%
     rename_all(longlat_rep)
+  if (export_as_csv== TRUE) {
+    write_csv(json, path = paste0(stringr::str_sub(jsonl_path,end = -7), ".csv"))
+  } else {
+    return(json)
+  }
 }
--- tests/testthat/test-parse_jsonl.R
+++ tests/testthat/test-parse_jsonl.R
@@ -1,6 +1,7 @@
 context("parse_jsonl")
 test_that("parse_jsonl works as expected", {
-result <- parse_jsonl("sample_data/test_jsonl_file.jsonl")
-check <- read.csv("sample_data/test_jsonl_file_fromGH.csv", stringsAsFactors = F)
+result <- {test_parse <- parse_jsonl("sample_data/test_jsonl_file.jsonl")
+c(nrow(test_parse),ncol(test_parse))}
+check <- c(520,99)
 expect_equal(result,check)
 })
