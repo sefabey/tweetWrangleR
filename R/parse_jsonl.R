062deff3e7fdfc5a709934cebeef2f5059e42ede
Sefa Ozalp <sefabey@gmail.com>
2018-08-24 19:05:25 +0100

    add regex method for parse_jsonl's tweet_lang argument, add new examples

--- R/parse_jsonl.R
+++ R/parse_jsonl.R
@@ -3,9 +3,9 @@
 #' Takes a .jsonl file, CONTINUE THE DOCUMENT
 #'
 #' @param jsonl_path Character. The path to the .jsonl file. It's a good idea to use the output of list.files() as an input.
-#' @param tweet_colnames Character vector. The colnames you want in the output tibble. In the object tweetWrangleR::tweet_cols, 99 opinionated colnames provided as default but users can provide a custom colname vector by add new ones or drop existing ones.
-#' @param tweet_lang Character. Filter tweets only in one language. Default is "en" for English language. Users can provide other languages as well.
-#' @param filter_term_regex Character. Accepts regex. Default is "muslim|islam". You can the keyword(s) you've used betwen quotation marks while filtering data. If you want to provide multiple keywords, put them between speech marks and separate by '|' (which means 'or').
+#' @param tweet_colnames Character vector. The colnames you want in the output tibble. In the object tweetWrangleR::tweet_cols, 99 opinionated colnames provided as default columns. However, customisation is possible by providing a user defined column names vectors.
+#' @param tweet_lang Character. Accepts regex. Not case sensitive. Filter tweets from selected languages. Default returns tweets from all languages. For English language tweets only, use "en". For multiple languages use regex or (e.g. "en|es|ar"). Users can use ISO 639-1 alpha-2 ('en'), ISO 639-3 alpha-3 ('msa'), or ISO 639-1 alpha-2 combined with an ISO 3166-1 alpha-2 localization ('zh-tw') formats.
+#' @param filter_term_regex Character. Accepts regex. Not case sensitive. Default returns all tweets. You can pass the keyword(s) you've used quotation marks while filtering data. If you want to provide multiple keywords, put them between speech marks and separate by '|' (which means 'or').
 #' @param export_as_csv Logical. If **TRUE**, it will export parsed data as a csv file within the same path as the .jsonl file. If **FALSE** (default), it will return a tibble.
 #'
 #' @return Always returns a tibble with the columns provided in the tweet_colnames argument. Default alwayns returns a tibble with 99 columns. If there are values that are missing in the source .jsonl file, fills NA.
@@ -14,10 +14,23 @@
 #'
 #' @examples parse_jsonl("user/desktop/tweets.jsonl")
 #'
-parse_jsonl <- function (jsonl_path, tweet_colnames=tweet_cols, tweet_lang='en', filter_term_regex="muslim|islam", export_as_csv=FALSE) {
+#' @examples parse_jsonl("user/desktop/tweets.jsonl",
+#' filter_term_regex = "this|that", tweet_lang = "en"))
+#'
+#' @examples
+#' **Parallel Example**
+#' input_path <- "user/where/your/jsonl/files/are"
+#' files <- list.files (input_path, full.names=TRUE, recursive=TRUE)
+#' parallel::mclapply(files, safely(parse_jsonl), export_as_csv=TRUE,mc.cores=3)
+#' files_csv <- list.files(input_path,full.names = TRUE, recursive = TRUE,pattern = ".csv$")
+#' whole_data <- map_df(files_csv, .f =read_csv,  col_types= "cccciclc?ddcccciilcccciiiccclciilccilciccclddcccccciiiccciiillccciccclddccciiccciiicccilclccicicccc")
+#'
+#'
+#'
+parse_jsonl <- function (jsonl_path, tweet_colnames=tweet_cols, tweet_lang=".", filter_term_regex=".", export_as_csv=FALSE) {
   stopifnot(stringr::str_detect(jsonl_path,"\\.jsonl$"))
   json <- ndjson::stream_in(jsonl_path, cls = "tbl") %>%
-    filter(lang==tweet_lang) %>%
+    filter(stringr::str_detect(string = lang, pattern = regex(tweet_lang,ignore_case = T))) %>%
     distinct(id_str,.keep_all = TRUE) %>%
     fncols(cname = tweet_colnames) %>%
     select_at(tweet_colnames) %>%
--- tests/testthat/test-parse_jsonl.R
+++ tests/testthat/test-parse_jsonl.R
@@ -1,7 +1,7 @@
 context("parse_jsonl")
 test_that("parse_jsonl works as expected", {
-result <- {test_parse <- parse_jsonl("sample_data/test_jsonl_file.jsonl")
+result <- {test_parse <- parse_jsonl("sample_data/test_jsonl_file.jsonl", filter_term_regex = "muslim|islam", tweet_lang = "en")
 c(nrow(test_parse),ncol(test_parse))}
-check <- c(520,99)
+check <- c(520,99) # expect 520 rows and 99 columns
 expect_equal(result,check)
 })
